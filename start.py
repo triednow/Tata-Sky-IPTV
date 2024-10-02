e2bffa5333d361eb10799fc6773024bc7cd05889
triednow <158821779+triednow@users.noreply.github.com>
2024-10-02 10:57:04 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,10 +1,13 @@
 from pyrogram.handlers import MessageHandler, CallbackQueryHandler, EditedMessageHandler
 from code_samples.bt import ButtonMaker
-def start(client,message):
-  message.reply_text("Hello Welcome How are You✨ This Is A Tataplay Access Token Generator Made By @aryanchy451")
+async def start(client,message):
+  await message.reply_text("Hello Welcome How are You✨ This Is A Tataplay Access Token Generator Made By @aryanchy451")
   buttons = ButtonMaker()
   buttons.ibutton("LOGIN WITH PASSWORD",'PASS')
   buttons.ibutton("LOGIN WITH OTP", 'OTP')
+  reply_markup = buttons.build_menu(2)
+  msg = "Select How To Login To Tataplay"
+  return await sendMessage(message, msg, reply_markup)
 
 
 async def main():
